037f0ac1c64e14cdf4be843a63edeb1587598d2d
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2020-09-23 20:39:15 +0600

    updated

--- app/User.php
+++ app/User.php
@@ -10,29 +10,17 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+
     protected $fillable = [
         'first_name','last_name','username', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
